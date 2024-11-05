bb9f94e8288a88f02dba402b6e774559f924f24d
yahuya <yahyapradika87@gmail.com>
2024-11-05 15:08:34 +0700

    Revert "adjust screen"
    This reverts commit 964310d0ba99418a261224bccd449a5e6e9516c5.

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,6 +1,6 @@
 const Banner = () => {
     return (
-        <div className="relative flex items-center justify-center h-96 bg-gradient-to-r from-blue-500 to-purple-600 text-white text-center p-8 shadow-lg overflow-hidden">
+        <div className="relative flex items-center justify-center h-96 bg-gradient-to-r from-blue-500 to-purple-600 text-white text-center p-8 rounded-lg shadow-lg overflow-hidden">
             {/* Background Image */}
             <img
                 src="https://fastly.picsum.photos/id/24/4855/1803.jpg?hmac=ICVhP1pUXDLXaTkgwDJinSUS59UWalMxf4SOIWb9Ui4" // URL gambar latar belakang
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,17 +12,6 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-      screens: {
-        'phone':'480px',
-        'tablet': '640px',
-        // => @media (min-width: 640px) { ... }
-
-        'laptop': '1024px',
-        // => @media (min-width: 1024px) { ... }
-
-        'desktop': '1280px',
-        // => @media (min-width: 1280px) { ... }
-      },
     },
   },
   plugins: [],
