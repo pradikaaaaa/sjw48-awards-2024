964310d0ba99418a261224bccd449a5e6e9516c5
yahuya <yahyapradika87@gmail.com>
2024-11-05 14:51:56 +0700

    adjust screen

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -26,7 +26,7 @@ const Header = () => {
   ]
 
   return (
-    <header className="bg-white">
+    <header className="bg-white border">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
--- components/Section/SectionOne.tsx
+++ components/Section/SectionOne.tsx
@@ -1,7 +1,7 @@
 const SectionOne = () => {
     return (
         <section className="flex items-center justify-center h-64 text-black text-center bg-white">
-            <div className="sm:mx-4 rounded-full border-solid border-2 border-indigo-600 p-8">
+            <div className="sm:mx-4 phone:mx-4 rounded-full border-solid border-2 border-indigo-600 p-8">
                 <h4>
                     Saatnya untuk menentukan siapakah fanbase yang akan berada di <span className="font-bold">PUNCAK</span>
                 </h4>
