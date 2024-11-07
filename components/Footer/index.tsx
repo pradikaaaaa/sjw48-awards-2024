9e3696189ce1c87561f86e96df97bec885fed730
yahuya <yahyapradika87@gmail.com>
2024-11-07 08:41:42 +0700

    adjust footer color

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,6 +1,16 @@
+'use client'
+
+import { usePathname } from "next/navigation"
+
 const Footer = () => {
+    const pathname = usePathname()
+    const isVote = pathname === '/vote'
+    const isVotePage = pathname === '/vote/page'
+
+    // console.log('ini pathname ==>', pathname)
+
     return (
-        <footer data-theme="light" className="bg-transparent w-full flex items-center justify-center">
+        <footer className={`${isVote ? "bg-gradient-to-r from-[#FF5858] to-[#FFC8C8]" : isVotePage ? "bg-gradient-to-r from-[#FF5858] to-[#FFC8C8] to-70%" :"bg-white"} w-full flex items-center justify-center`}>
             <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
                 <span className="text-sm text-black">&copy; 2024</span>
             </div>
