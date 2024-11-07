cb38b391fba21f683d91b190c8de2257fc6ac5e1
yahuya <yahyapradika87@gmail.com>
2024-11-07 09:00:14 +0700

    fix type data

--- app/vote/page/page.tsx
+++ app/vote/page/page.tsx
@@ -1,27 +1,35 @@
 'use client'
 
-import { useState} from 'react'
+import { useState, useEffect} from 'react'
 import CandidateCard from "@/components/Vote/CandidateCard"
 import VoteSection from "@/components/Vote/VoteSection"
 import fanbaseList from "@/data/fanbase";
 
 const VotePage = () => {
+    const [maxVote, setMaxVote] = useState(0)
 
     const [votes, setVotes] = useState(
-        fanbaseList.reduce((acc, candidate) => {
+        fanbaseList.reduce((acc : {[key:number]:number}, candidate) => {
             acc[candidate.id] = 0;
             return acc;
         }, {})
     )
 
-    const handleVoteChange = (candidateId, newVoteCount) => {
-        setVotes(prevVotes => ({
-            ...prevVotes,
-            [candidateId]: newVoteCount,
-        }))
+    const handleVoteChange = (candidateId : number, newVoteCount : number) => {
+        if(totalVotes < maxVote ){
+            setVotes(prevVotes => ({
+                ...prevVotes,
+                [candidateId]: newVoteCount,
+            }))
+        }
+       
     }
 
     const totalVotes = Object.values(votes).reduce((sum, count) => sum + count, 0);
+
+    useEffect(()=>{
+        setMaxVote(7)
+    },[])
 
     return (
         <div className="min-h-screen bg-gradient-to-r from-[#FF5858] to-[#FFC8C8] to-70% p-8">
@@ -30,7 +38,7 @@ const VotePage = () => {
             <div className="flex flex-col md:flex-row">
                 {/* Bagian VoteSection */}
                 <div className="static md:w-1/4 mb-6 md:mb-0 md:mr-6">
-                    <VoteSection voteTerpakai={totalVotes} jumlahVotes={30} />
+                    <VoteSection voteTerpakai={totalVotes} jumlahVotes={maxVote} />
                 </div>
 
                 {/* Bagian Kandidat */}
