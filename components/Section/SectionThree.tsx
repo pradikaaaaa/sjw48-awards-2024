5bd9a199c0c8f815d422beeacc17a0107b6ebd5f
yahuya <pradikanizer13@gmail.com>
2024-11-23 11:02:00 +0700

    commit 23/11/2024

--- components/Section/SectionThree.tsx
+++ components/Section/SectionThree.tsx
@@ -1,23 +1,59 @@
+"use client";
+
 import React from "react";
 
-const ItemCard = ({ title, subtitle, buttonLabel } : {title:string, subtitle:string, buttonLabel: string}) => (
+const ItemCard = ({
+  title,
+  subtitle,
+  buttonLabel,
+  url_cat,
+}: {
+  title: string;
+  subtitle: string;
+  buttonLabel: string;
+  url_cat: string;
+}) => (
   <div className="flex justify-between items-center p-4 bg-white shadow-md rounded-lg mb-4">
     <div>
       <h2 className="text-lg text-black font-bold">{title}</h2>
       <p className="text-sm text-gray-700 font-semibold mt-1">{subtitle}</p>
     </div>
-    <button className="bg-[#1D5D83] text-white font-semibold px-4 py-2 rounded-md shadow-md">
+    <a
+      href={url_cat}
+      target="_blank"
+      className="bg-[#1D5D83] text-white font-semibold px-4 py-2 rounded-md shadow-md"
+    >
       {buttonLabel}
-    </button>
+    </a>
   </div>
 );
 
 const VoteList = () => {
   const items = [
-    { title: "CAT 1 SJW48 Awards 2024", subtitle: "250 Suara", buttonLabel: "PESAN" },
-    { title: "CAT 2 SJW48 Awards 2024", subtitle: "75 Suara", buttonLabel: "PESAN" },
-    { title: "CAT 6 SJW48 Awards 2024", subtitle: "10 Suara", buttonLabel: "PESAN" },
-    { title: "CAT 8 SJW48 Awards 2024", subtitle: "5 Suara", buttonLabel: "PESAN" },
+    {
+      title: "CAT 1 SJW48 Awards 2024",
+      subtitle: "250 Suara",
+      buttonLabel: "PESAN",
+      url_cat : "https://wa.me/6285183184848"
+    },
+    {
+      title: "CAT 2 SJW48 Awards 2024",
+      subtitle: "75 Suara",
+      buttonLabel: "PESAN",
+      url_cat : "https://tiptip.id/"
+    },
+    {
+      title: "CAT 6 SJW48 Awards 2024",
+      subtitle: "10 Suara",
+      buttonLabel: "PESAN",
+      url_cat : "https://tiptip.id/"
+    },
+    {
+      title: "CAT 8 SJW48 Awards 2024",
+      subtitle: "5 Suara",
+      buttonLabel: "PESAN",
+      url_cat : "https://tiptip.id/"
+    },
   ];
 
   return (
--- components/Vote/Form/VotingPage.tsx
+++ components/Vote/Form/VotingPage.tsx
@@ -1,136 +1,163 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react'
-import CandidateCard from "@/components/Vote/Form/CandidateCard"
-import VoteSection from "@/components/Vote/Form/VoteSection"
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import CandidateCard from "@/components/Vote/Form/CandidateCard";
+import VoteSection from "@/components/Vote/Form/VoteSection";
 
-import api from '@/services/api';
-import { Fanbase } from '@/types/fanbase';
-import { VoteFanbase } from '@/types/vote_fanbase';
-import ModalKonfirmasiVote from './ModalKonfirmasiVote';
-import ModalSelesaiVote from './ModalSelesaiVote';
+import api from "@/services/api";
+import { Fanbase } from "@/types/fanbase";
+import { VoteFanbase } from "@/types/vote_fanbase";
+import ModalKonfirmasiVote from "./ModalKonfirmasiVote";
+import ModalSelesaiVote from "./ModalSelesaiVote";
+import ModalError from "@/components/Modal/ModalError";
 
+const VotingPage = ({
+  maxVotes,
+  listIdVote,
+}: {
+  maxVotes: number;
+  listIdVote: number[];
+}) => {
+    const router = useRouter()
 
+  const [maxVote, setMaxVote] = useState(0);
+  const [nominasi, setNominasi] = useState<Fanbase[]>([]);
+  const [openModalKonfirmasi, setOpenModalKonfirmasi] = useState(false);
+  const [openModalSelesai, setOpenModalSelesai] = useState(false);
 
-const VotingPage = ({maxVotes, listIdVote}:{maxVotes:number, listIdVote:number[]}) => {
-    const [maxVote, setMaxVote] = useState(0)
-    const [nominasi, setNominasi] = useState<Fanbase[]>([])
-    const [openModalKonfirmasi, setOpenModalKonfirmasi] = useState(false)
-    const [openModalSelesai, setOpenModalSelesai] = useState(false)
+  const [openModalError, setOpenModalError] = useState(false);
+  const [messageError, setMessageError] = useState("");
 
-    const [votes, setVotes] = useState<VoteFanbase[]>([])
+  const [votes, setVotes] = useState<VoteFanbase[]>([]);
 
-    const handleVoteChange = (candidateId: number, newVoteCount: number) => {
-       setVotes(prevVotes =>
-            prevVotes.map(item =>
-                item.id === candidateId ? { ...item, jumlah_vote : newVoteCount} : item
-            )
-       )
-    }
-
-    const handleVoteKonfirmasi = (open : boolean) => {
-        setOpenModalKonfirmasi(open)
-    }
-
-    useEffect(() => {
-        getData()
-        setMaxVote(maxVotes)
-    }, [])
-
-    const getData =  async () => {
-        const endPoint = `/nominasi`
-        try{
-            const response = await api.create().apiGET({endPoint})
-            setNominasi(response.data)
-            const _votes = response.data.map((item:Fanbase)=>({
-                id : item.id,
-                jumlah_vote : 0,
-                nama : item.nama
-            }))
-            setVotes(_votes)
-        }catch(error){
-            console.warn(error)
-        }
-    }
-
-    const voteItem = (id : number) : number => {
-        const selectedVote = votes.find(item => item.id === id)
-        return selectedVote ? selectedVote.jumlah_vote : 0
-    }
-
-    const getTotalVotes = () => {
-        return votes.reduce((total, item) => total + item.jumlah_vote, 0);
-    };
-
-    const totalVotes = getTotalVotes()
-    
-    const getSelectedNominasi = () => {
-        return votes.filter(item => item.jumlah_vote !== 0);
-    };
-
-    const selectedNominasi = getSelectedNominasi()
-
-    //
-    const handleSubmitVote = async () => {
-        const endPoint = `/vote/process`
-        const request = {
-            "id_kode" : listIdVote,
-            "vote" : selectedNominasi 
-        }
-    
-        try{
-            const response = await api.create().apiPOST({endPoint, request})
-            if(response.status == '200'){
-                setOpenModalSelesai(true)
-            }
-        }catch(error){
-            console.warn(error)
-        }
-    }
-
-    return (
-        <div className="min-h-screen bg-gradient-to-r from-[#FF5858] to-[#FFC8C8] p-8">
-            <h1 className="text-center text-2xl font-bold mb-6">PILIH KANDIDAT</h1>
-            <p className="text-center mb-4 text-sm">*Tambahkan jumlah poin suara untuk fanbase yang kamu dukung, kemudian klik tombol <b>VOTE</b></p>
-            <div className="flex flex-col md:flex-row">
-                {/* Bagian VoteSection */}
-                <div className="flex md:w-1/4 mb-6 md:mb-0 md:mr-6 xs:justify-center">
-                    <VoteSection 
-                        voteTerpakai={maxVote - totalVotes} 
-                        jumlahVotes={maxVote} 
-                        onClickVote={handleVoteKonfirmasi}
-                    />
-                </div>
-
-                {/* Bagian Kandidat */}
-                <div className="static md:w-3/4 grid grid-cols-2 md:grid-cols-4 gap-4 xl:mb-4 sm:mb-80 xs:mb-80">
-                    {nominasi.map((candidate, index) => (
-                        <CandidateCard
-                            key={index}
-                            id={candidate.id}
-                            name={candidate.nama}
-                            image={candidate.logo}
-                            voteCount={voteItem(candidate.id)}
-                            voteTerpakai={totalVotes}
-                            jumlahVote={maxVote}
-                            onVoteChange={handleVoteChange}
-                        />
-                    ))}
-                </div>
-            </div>
-
-            <ModalKonfirmasiVote 
-                status={openModalKonfirmasi} 
-                fanbaseList={selectedNominasi}
-                onClickVote={handleVoteKonfirmasi} 
-                onSubmitVote={handleSubmitVote}
-            />
-
-            <ModalSelesaiVote
-                status={openModalSelesai}
-            />
-        </div>
+  const handleVoteChange = (candidateId: number, newVoteCount: number) => {
+    setVotes((prevVotes) =>
+      prevVotes.map((item) =>
+        item.id === candidateId ? { ...item, jumlah_vote: newVoteCount } : item
+      )
     );
-}
+  };
 
-export default VotingPage
+  const handleVoteKonfirmasi = (open: boolean) => {
+    setOpenModalKonfirmasi(open);
+  };
+
+  useEffect(() => {
+    getData();
+    setMaxVote(maxVotes);
+  }, []);
+
+  const getData = async () => {
+    const endPoint = `/nominasi`;
+    try {
+      const response = await api.create().apiGET({ endPoint });
+      setNominasi(response.data);
+      const _votes = response.data.map((item: Fanbase) => ({
+        id: item.id,
+        jumlah_vote: 0,
+        nama: item.nama,
+      }));
+      setVotes(_votes);
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  const voteItem = (id: number): number => {
+    const selectedVote = votes.find((item) => item.id === id);
+    return selectedVote ? selectedVote.jumlah_vote : 0;
+  };
+
+  const getTotalVotes = () => {
+    return votes.reduce((total, item) => total + item.jumlah_vote, 0);
+  };
+
+  const totalVotes = getTotalVotes();
+
+  const getSelectedNominasi = () => {
+    return votes.filter((item) => item.jumlah_vote !== 0);
+  };
+
+  const selectedNominasi = getSelectedNominasi();
+
+  //
+  const handleSubmitVote = async () => {
+    const endPoint = `/vote/process`;
+    const request = {
+      id_kode: listIdVote,
+      vote: selectedNominasi,
+    };
+
+    try {
+      const response = await api.create().apiPOST({ endPoint, request });
+      if (response.status == "200") {
+        setOpenModalSelesai(true);
+      } else if (response.status == "204") {
+        setOpenModalError(true);
+        setMessageError(response.message);
+      }
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+
+  const handleModalError = (open: boolean) => {
+    setOpenModalError(open);
+    console.log('aaaaa')
+    router.push("/")
+  };
+
+  return (
+    <div className="min-h-screen bg-gradient-to-r from-[#FF5858] to-[#FFC8C8] p-8">
+      <h1 className="text-center text-2xl font-bold mb-6">PILIH KANDIDAT</h1>
+      <p className="text-center mb-4 text-sm">
+        *Tambahkan jumlah poin suara untuk fanbase yang kamu dukung, kemudian
+        klik tombol <b>VOTE</b>
+      </p>
+      <div className="flex flex-col md:flex-row">
+        {/* Bagian VoteSection */}
+        <div className="flex md:w-1/4 mb-6 md:mb-0 md:mr-6 xs:justify-center">
+          <VoteSection
+            voteTerpakai={maxVote - totalVotes}
+            jumlahVotes={maxVote}
+            onClickVote={handleVoteKonfirmasi}
+          />
+        </div>
+
+        {/* Bagian Kandidat */}
+        <div className="static md:w-3/4 grid grid-cols-2 md:grid-cols-4 gap-4 xl:mb-4 sm:mb-80 xs:mb-80">
+          {nominasi.map((candidate, index) => (
+            <CandidateCard
+              key={index}
+              id={candidate.id}
+              name={candidate.nama}
+              image={candidate.logo}
+              voteCount={voteItem(candidate.id)}
+              voteTerpakai={totalVotes}
+              jumlahVote={maxVote}
+              onVoteChange={handleVoteChange}
+            />
+          ))}
+        </div>
+      </div>
+
+      <ModalKonfirmasiVote
+        status={openModalKonfirmasi}
+        fanbaseList={selectedNominasi}
+        onClickVote={handleVoteKonfirmasi}
+        onSubmitVote={handleSubmitVote}
+      />
+
+      <ModalSelesaiVote status={openModalSelesai} />
+
+      <ModalError
+        status={openModalError}
+        message={messageError}
+        onCloseModal={handleModalError}
+      />
+    </div>
+  );
+};
+
+export default VotingPage;
