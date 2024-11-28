764f19f783bdb834a740b8d64889f605ebe4cbdd
yahuya <pradikanizer13@gmail.com>
2024-11-28 22:16:14 +0700

    fix footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,13 +4,14 @@ import { usePathname } from "next/navigation"
 
 const Footer = () => {
     const pathname = usePathname()
-    const isVote = pathname === '/vote-awards' || '/vote-fanbase'
+    const isVoteFanbase = pathname === '/vote-fanbase'
+    const isVoteNominasi = pathname === '/vote-awards'
     const isVotePage = pathname === '/vote/page'
 
     // console.log('ini pathname ==>', pathname)
 
     return (
-        <footer className={`${isVote ? "bg-gradient-to-r from-[#FF5858] to-[#FFC8C8]" : isVotePage ? "bg-gradient-to-r from-[#FF5858] to-[#FFC8C8] to-70%" :"bg-white"} w-full flex items-center justify-center`}>
+        <footer className={`${isVoteFanbase || isVoteNominasi ? "bg-gradient-to-r from-[#FF5858] to-[#FFC8C8]" : isVotePage ? "bg-gradient-to-r from-[#FF5858] to-[#FFC8C8] to-70%" :"bg-white"} w-full flex items-center justify-center`}>
             <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
                 <span className="text-sm text-black">&copy; 2024</span>
             </div>
