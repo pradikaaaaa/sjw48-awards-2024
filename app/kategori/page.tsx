2b9cb10289b91c14268a3e1f812d70618e80c701
Yahuya <pradikanizer13@gmail.com>
2024-12-01 19:04:31 +0700

    fix padding

--- app/kategori/page.tsx
+++ app/kategori/page.tsx
@@ -13,12 +13,12 @@ function SearchTipe() {
 
   return <>{filteredData.map((category) => (
     <>
-      <div>
+      <div className="pt-3">
         <h2 className="text-3xl font-bold mx-8 text-black">
           {category.nama}
         </h2>
         <hr className="mx-6 mt-1" />
-        <section className="flex item-center justify-center h-full mt-4 mb-6 pb-6 mx-4 bg-white">
+        <section className="flex item-center justify-center h-full mt-4 pb-6 mx-4 bg-white">
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 2xs:grid-cols-2 xs:grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 xl:gap-x-8">
             {category.nominasi.map((nominasi) => (
               <>
