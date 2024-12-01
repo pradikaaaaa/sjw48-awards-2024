f39907fa55d7da0ed5da66d147060e8348670c6a
rayhan0898 <rayhan0898@hotmail.com>
2024-12-01 14:22:12 +0700

    optimisasi-load

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
     return (
         <footer className={`${isVoteFanbase || isVoteNominasi ? "bg-gradient-to-r from-[#FF5858] to-[#FFC8C8]" : isVotePage ? "bg-gradient-to-r from-[#FF5858] to-[#FFC8C8] to-70%" :"bg-white"} w-full flex items-center justify-center`}>
             <div className="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-center">
-                <span className="text-sm text-black">&copy; 2024</span>
+                <span className="text-sm text-black">Copyright &copy; 2024 SJW48 Operation Team</span>
             </div>
         </footer>
     )
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
+// https://static.miraheze.org
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lieterasi.sjw48.com',
+        port: '',
+        // pathname: '/my-bucket/**',
+      },
+    ],
+  },
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+
 
 export default nextConfig;
