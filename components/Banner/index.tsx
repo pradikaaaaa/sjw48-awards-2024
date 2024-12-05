72beab60c8aab6741f8089f275a5972236045aa9
yahuya <pradikanizer13@gmail.com>
2024-12-05 15:58:13 +0700

    add new banner

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -5,19 +5,41 @@ const Banner = () => {
         <div className="relative z-0 flex items-center 
             justify-center 
             h-[541px] 
-            2xs:h-[108.2px]
-            xs:h-[162.3px]
-            sm:h-[216.4px] 
-            md:h-[259.7px] 
+            2xs:h-[320px]
+            xs:h-[480px]
+            sm:h-[234px] 
+            md:h-[380px] 
             lg:h-[541px] 
             xl:h-[541px] 
             text-white bg-white text-center p-8 overflow-hidden">
             {/* Background Image */}
             <Image 
                 fill={true}
-                src="/images/banner/banner.jpeg" // URL gambar latar belakang
+                src="/images/banner/banner-desktop.webp" // URL gambar latar belakang
                 alt="Background"
-                className="absolute inset-0 w-full h-full"
+                className="absolute inset-0 
+                w-full h-full
+                2xs:invisible
+                xs:invisible
+                sm:visible
+                md:visible
+                lg:visible
+                "
+            />
+
+            {/* Banner Mobile */}
+            <Image 
+                fill={true}
+                src="/images/banner/banner-mobile.webp"
+                alt="Background"
+                className="absolute 
+                    inset-0 
+                    w-full h-full
+                    xs:visible
+                    2xs:visible
+                    sm:invisible
+                    md:invisible
+                    lg:invisible"
             />
 
             {/* Banner Content */}
